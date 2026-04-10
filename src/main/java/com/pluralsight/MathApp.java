b9fe361850d8f1d6e7eb2757bc0998035b4f65ca
beatrizjardim031 <beatrizreisjardim@gmail.com>
2026-04-09 20:23:30 -0500

    Feat: Add question 7 and 8

--- src/main/java/com/pluralsight/MathApp.java
+++ src/main/java/com/pluralsight/MathApp.java
@@ -9,17 +9,17 @@ public class MathApp {
         double bobSalary = 1878.98;
         double highestSalary = Math.max(bobSalary,garySalary);
 
-        System.out.printf("The highest salary is: %.2f.%n%n", highestSalary);
+        System.out.printf("The highest salary is: $%.2f.%n%n", highestSalary);
 //  1.2
         double truckPrice = 48000.00;
         double carPrice = 18000.00;
         double smallestPrice = Math.min(truckPrice, carPrice);
 
-        System.out.printf("The smallest price is: %.2f.%n%n", smallestPrice);
+        System.out.printf("The smallest price is: $%.2f.%n%n", smallestPrice);
 
 //  1.3
-        double radius = Math.PI;
-        double circleArea = Math.pow(radius, 2) * radius;
+        double radius = 7.25;
+        double circleArea = Math.pow(radius, 2) * Math.PI;
 
         System.out.printf("The area of the circle is: %.2f.%n%n", circleArea);
 
@@ -35,8 +35,23 @@ public class MathApp {
         System.out.printf("The distance is: %.2f.%n%n", distance);
 
 //  1.6
-        double absolute = Math.abs(-3.5);
+        double absolute = Math.abs(-3.8);
 
         System.out.printf("The absolute is: %.1f.%n%n", absolute);
+
+//  1.7
+        double randomNumber = Math.random();
+
+        System.out.printf("Your random number is: %.2f.%n%n", randomNumber);
+
+//  1.8
+        int hourMin = 60;
+        int dayHours = 24;
+        int days = 24;
+        int totalMin = dayHours * hourMin * days;
+
+        System.out.printf("24 days have: %d minutes.%n%n", totalMin);
+
+
     }
 }
